dc5eee8da970eefe060be66040d6f2bada7e080e
Aziztampan <dokterfarma001@gmail.com>
2023-11-26 02:04:01 +0700

    init

---
+++ src/components/post/CreatePost.jsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../provider/AuthProvider";
+import InputField from "./components/InputField";
+import { Profile } from "./components/Profile";
+import { iconPosting } from "./faker/iconButton";
+import { useRouter } from "next/navigation";
+
+const CreatePost = () => {
+  const [auth, setAuth] = useState();
+  const [loading, setLoading] = useState(false);
+  const [post, setPost] = useState("");
+
+  const router = useRouter();
+
+  const handlePost = async () => {
+    if (!post) {
+      return alert("You can not Post with empty feel");
+    }
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://devscale-mockapi.fly.dev/api/collections/notes/records",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user: auth,
+            content: post,
+            additionalData: "4212ganteng",
+          }),
+        }
+      );
+
+      setPost("");
+
+      setLoading(false);
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const isLogedin = JSON.parse(localStorage.getItem("auth"));
+
+    if (isLogedin) {
+      setAuth(isLogedin.name);
+    } else {
+      return router.push("/");
+    }
+  }, []);
+  return (
+    <div className="card-global ">
+      <div className=" gap-4 space-y-3">
+        <Profile />
+        <InputField
+          handleChange={(e) => setPost(e.target.value)}
+          value={post}
+        />
+      </div>
+      <hr className="my-4" />
+
+      <div className=" space-y-10 ">
+        <div className="lg:flex lg:mt-7  lg:justify-between space-y-3 lg:items-center lg:border lg:rounded-lg lg:px-2 lg:py-4  ">
+          <div>
+            <h1 className="">Tambah ke Postingan Anda</h1>
+          </div>
+          <div className="lg:flex ">
+            {iconPosting.map((item, index) => (
+              <div className="flex space-x-2 items-center " key={index}>
+                <button className="hover:bg-slate-100 hover:rounded-full lg:w-full h-full  p-2">
+                  {item?.icon}
+                </button>
+                <h1 className="lg:hidden">{item.name}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          className="bg-sky-500 text-base  w-full text-slate-200  py-3 rounded-md font-bold hover:bg-sky-600 hover:text-slate-100"
+          onClick={handlePost}
+        >
+          {loading ? "loading..." : "Post"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
---
+++ src/components/post/ListPost.jsx
@@ -0,0 +1,128 @@
+"use client";
+
+import profile from "@/assets/aziz.jpeg";
+import Image from "next/image";
+import { useState } from "react";
+import InputField from "./components/InputField";
+import { FileEdit, ThumbsUp, Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const ListPost = ({ user, content, id }) => {
+  const [isEdit, setEdit] = useState(false);
+  const [post, setPost] = useState(content);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      setLoading(true);
+      await fetch(
+        `https://devscale-mockapi.fly.dev/api/collections/notes/records/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      setLoading(false);
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdate = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://devscale-mockapi.fly.dev/api/collections/notes/records/${id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ content: post, user: user }),
+        }
+      );
+      setLoading(false);
+      setEdit(false);
+      router.refresh();
+      const data = await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+
+      <div className="card-global space-y-4" key={{ id }}>
+        <div className="flex space-x-3 items-center">
+          <Image
+            src={profile}
+            className="rounded-full"
+            width={60}
+            alt="profile"
+          />
+          <div className="">
+            <h1 className="font-bold">{user}</h1>
+            <input
+              type="text"
+              value={"Sponsored"}
+              className="text-slate-400"
+              disabled
+            />
+          </div>
+        </div>
+
+        <div className="border border-slate-100 px-3 py-1 min-h-[100px]">
+          {isEdit ? (
+            <InputField
+              value={post}
+              handleChange={(e) => setPost(e.target.value)}
+            />
+          ) : (
+            <div>{content}</div>
+          )}
+        </div>
+
+        <div className="flex justify-between items-center px-3">
+          <button className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg">
+            <ThumbsUp />
+            <span>Like</span>
+          </button>
+
+          {!isEdit ? (
+            <button
+              className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
+              onClick={() => setEdit(true)}
+            >
+              <FileEdit />
+              <span>Edit</span>
+            </button>
+          ) : (
+            <button
+              className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
+              onClick={handleUpdate}
+            >
+              <FileEdit />
+              <span>Update</span>
+            </button>
+          )}
+
+          <button
+            className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
+            onClick={handleDelete}
+          >
+            <Trash2 />
+            <span>Delete</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListPost;
