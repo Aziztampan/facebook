c5292fdf9ff562c40d2f215d1d578e6aa8d8cf2f
Aziztampan <dokterfarma001@gmail.com>
2023-11-26 02:06:36 +0700

    feat:list post - adding loading

--- src/components/post/ListPost.jsx
+++ src/components/post/ListPost.jsx
@@ -54,73 +54,75 @@ const ListPost = ({ user, content, id }) => {
 
   return (
     <>
-      <div className="loading">
-        <h1>Loading...</h1>
-      </div>
-
-      <div className="card-global space-y-4" key={{ id }}>
-        <div className="flex space-x-3 items-center">
-          <Image
-            src={profile}
-            className="rounded-full"
-            width={60}
-            alt="profile"
-          />
-          <div className="">
-            <h1 className="font-bold">{user}</h1>
-            <input
-              type="text"
-              value={"Sponsored"}
-              className="text-slate-400"
-              disabled
+      {loading ? (
+        <div className="loading">
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div className="card-global space-y-4" key={{ id }}>
+          <div className="flex space-x-3 items-center">
+            <Image
+              src={profile}
+              className="rounded-full"
+              width={60}
+              alt="profile"
             />
+            <div className="">
+              <h1 className="font-bold">{user}</h1>
+              <input
+                type="text"
+                value={"Sponsored"}
+                className="text-slate-400"
+                disabled
+              />
+            </div>
+          </div>
+
+          <div className="border border-slate-100 px-3 py-1 min-h-[100px]">
+            {isEdit ? (
+              <InputField
+                value={post}
+                handleChange={(e) => setPost(e.target.value)}
+              />
+            ) : (
+              <div>{content}</div>
+            )}
+          </div>
+
+          <div className="flex justify-between items-center px-3">
+            <button className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg">
+              <ThumbsUp />
+              <span>Like</span>
+            </button>
+
+            {!isEdit ? (
+              <button
+                className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
+                onClick={() => setEdit(true)}
+              >
+                <FileEdit />
+                <span>Edit</span>
+              </button>
+            ) : (
+              <button
+                className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
+                onClick={handleUpdate}
+              >
+                <FileEdit />
+                <span>Update</span>
+              </button>
+            )}
+
+            <button
+              className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
+              onClick={handleDelete}
+            >
+              <Trash2 />
+              <span>Delete</span>
+            </button>
           </div>
         </div>
-
-        <div className="border border-slate-100 px-3 py-1 min-h-[100px]">
-          {isEdit ? (
-            <InputField
-              value={post}
-              handleChange={(e) => setPost(e.target.value)}
-            />
-          ) : (
-            <div>{content}</div>
-          )}
-        </div>
-
-        <div className="flex justify-between items-center px-3">
-          <button className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg">
-            <ThumbsUp />
-            <span>Like</span>
-          </button>
-
-          {!isEdit ? (
-            <button
-              className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
-              onClick={() => setEdit(true)}
-            >
-              <FileEdit />
-              <span>Edit</span>
-            </button>
-          ) : (
-            <button
-              className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
-              onClick={handleUpdate}
-            >
-              <FileEdit />
-              <span>Update</span>
-            </button>
-          )}
-
-          <button
-            className="flex space-x-2 hover:bg-slate-100 p-2 rounded-lg"
-            onClick={handleDelete}
-          >
-            <Trash2 />
-            <span>Delete</span>
-          </button>
-        </div>
-      </div>
+      )}
     </>
   );
 };
